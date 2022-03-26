e0bfc56312e3c7d89e26144cadad8e5c7afa898b
RamiroSchenone <ramacanalla21@gmail.com>
2022-03-26 03:02:21 -0300

    *23. @input recibir info de padre a hijo (spa)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { app_routing } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+import { HeroesService } from './services/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    FooterComponent,
+    HeroeComponent,
+    BuscadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
-  providers: [],
+  providers: [
+    HeroesService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe, HeroesService } from 'src/app/services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+  termino: string = "";
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private router: Router,
+    private heroesService: HeroesService
+  ) { }
+
+  ngOnInit(): void {
+    this.ActivatedRoute.params
+    .subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this.heroesService.searchHeroes(params ['termino']);
+      console.log(this.heroes)
+    })
+  }
+
+  getHeroe( index: number ){
+    this.router.navigate(['heroes', index])
+  }
+}
